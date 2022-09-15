6102b76c696d9dc3b4a1f7dac6f42766d55e414f
Kristina Pyzhenkova <pyzhenkova-k@mail.ru>
2022-09-15 16:25:04 +0300

    finall

--- item_django_stripe/item_django_stripe/settings.py
+++ item_django_stripe/item_django_stripe/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='123456789')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-environ.Env.read_env(env_file=str(BASE_DIR) + '/item_django_stripe/.env')
+environ.Env.read_env()
 
 DEBUG = True
 
