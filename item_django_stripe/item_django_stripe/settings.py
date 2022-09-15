cee0ca233f6c53916f8c0ecb3db03f8b381377ef
Kristina Pyzhenkova <pyzhenkova-k@mail.ru>
2022-09-15 15:26:08 +0300

    finall

--- item_django_stripe/item_django_stripe/settings.py
+++ item_django_stripe/item_django_stripe/settings.py
@@ -99,5 +99,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', default='123456789')
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', default='123456789')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
